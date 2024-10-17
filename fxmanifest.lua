4ba5c03ae1ead9246c914086674b3b10a05df5f9
VKTDEVELOPMENT <164358696+Valkata4235421@users.noreply.github.com>
2024-10-18 01:23:02 +0300

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,18 +1,38 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
+
 author 'VKT DEVELOPMENT'
-description 'qb-input design by VKT DEVELOPMENT'
+description 'progressbar design by VKT DEVELOPMENT'
 version '1.0.0'
 
-client_scripts {
-    'client/*.lua'
-}
+ui_page "html/index.html"
 
-ui_page 'html/index.html'
+client_script "client/main.lua"
 
 files {
     'html/index.html',
-    'html/styles/*.css',
-    'html/script.js'
+    'html/css/style.css',
+    'html/js/script.js',
+
+    'html/css/bootstrap.min.css',
+    'html/js/jquery.min.js',
+}
+
+exports {
+    'Progress',
+    'ProgressWithStartEvent',
+    'ProgressWithTickEvent',
+    'ProgressWithStartAndTick'
+}
+
+escrow_ignore {
+    'html/index.html',
+    'html/css/style.css',
+    'html/js/script.js',
+
+    'html/css/bootstrap.min.css',
+    'html/js/jquery.min.js',
+
+    'client/main.lua'
 }
